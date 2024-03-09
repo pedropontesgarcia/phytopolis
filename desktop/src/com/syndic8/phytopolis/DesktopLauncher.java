7bdfb0b7c32aff5eed728a03bb248fd3bff2dd3c
Pedro Pontes García <pp457@cornell.edu>
2024-03-09 18:41:39 -0500

    Empty project

---
+++ desktop/src/com/syndic8/phytopolis/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.syndic8.phytopolis;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.syndic8.phytopolis.GDXRoot;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("Phytopolis");
+		new Lwjgl3Application(new GDXRoot(), config);
+	}
+}
