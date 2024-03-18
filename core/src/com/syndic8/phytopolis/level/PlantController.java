98ea86092a36a174fb34e3a992c7b86db35fcc6f
Jordan-Rudolph <chubbyseventeen17@gmail.com>
2024-03-18 13:07:44 -0400

    Add PlantController implemented with Branch and Leaf classes

--- core/src/com/syndic8/phytopolis/level/PlantController.java
+++ core/src/com/syndic8/phytopolis/level/PlantController.java
@@ -1,2 +1,424 @@
-package com.syndic8.phytopolis.level;public class PlantController {
+package com.syndic8.phytopolis.level;
+
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.World;
+import com.syndic8.phytopolis.GameCanvas;
+import com.syndic8.phytopolis.assets.AssetDirectory;
+
+public class PlantController {
+
+    /**
+     * the grid containing all possible nodes for a branch to grow from
+     */
+    private final PlantNode[][] plantGrid;
+    /**
+     * width of the plant grid
+     */
+    private final int width;
+    /**
+     * height of the plant grid
+     */
+    private final int height;
+    /**
+     * how far apart to space each node on screen
+     */
+    private final float gridSpacing;
+    /**
+     * how far apart each node is on the x axis
+     */
+    private final float xSpacing;
+    /**
+     * x coordinate of the origin of this plant
+     */
+    private final float xOrigin;
+    /**
+     * y coordinate of the origin of this plant
+     */
+    private final float yOrigin;
+    /**
+     * world used for storing physics objects
+     */
+    private final World world;
+    private final Vector2 scale;
+    /**
+     * node texture
+     */
+    protected Texture nodeTexture;
+    /**
+     * branch texture
+     */
+    protected TextureRegion branchTexture;
+    /**
+     * leaf texture
+     */
+    protected TextureRegion leafTexture;
+
+    /**
+     * Initialize a PlantController with specified height and width
+     *
+     * @param height      the height of the plant grid
+     * @param width       the width of the plant grid
+     * @param gridSpacing the spacing between nodes of the plant
+     * @param world       world to assign physics objects to
+     * @param xOrigin     x origin of the plant
+     * @param yOrigin     y origin of the plant
+     */
+    public PlantController(int width,
+                           int height,
+                           float gridSpacing,
+                           float xOrigin,
+                           float yOrigin,
+                           World world,
+                           Vector2 scale) {
+        plantGrid = new PlantNode[width][height];
+        this.world = world;
+        this.xOrigin = xOrigin;
+        this.yOrigin = yOrigin;
+        this.width = width;
+        this.height = height;
+        this.gridSpacing = gridSpacing;
+        this.xSpacing = (float) Math.sqrt((gridSpacing * gridSpacing) -
+                ((gridSpacing / 2f) *
+                        (gridSpacing / 2f)));
+        this.scale = scale;
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                float yOffset = 0;
+                //set the yOffset
+                if (x % 2 == 1) yOffset = gridSpacing / 2f;
+                plantGrid[x][y] = new PlantNode((x * xSpacing) + xOrigin,
+                        yOrigin + yOffset +
+                                (y * gridSpacing));
+            }
+        }
+    }
+
+    /**
+     * grows a branch at the desired position
+     *
+     * @param x         the x coordinate of the node to grow the branch at
+     * @param y         the y coordinate of the node to grow the branch at
+     * @param direction the direction in which to grow the branch
+     * @param type      the type of branch to grow
+     */
+    public void growBranch(float x,
+                                      float y,
+                                      branchDirection direction,
+                                      branchType type) {
+        int xIndex = xCoordToIndex(x * 10);
+        int yIndex = yCoordToIndex(y * 10);
+        plantGrid[xIndex][yIndex].makeBranch(direction,
+                type,
+                branchTexture,
+                world);
+    }
+
+    public void growLeaf(float x, float y, leafType type){
+        int xIndex = xCoordToIndex(x * 10);
+        int yIndex = yCoordToIndex(y * 10);
+        plantGrid[xIndex][yIndex].makeLeaf(type, leafTexture, world);
+    }
+
+    /**
+     * destroys a branch at the desired position
+     *
+     * @param x         the x coordinate of the node to destroy the branch at
+     * @param y         the y coordinate of the node to destroy the branch at
+     * @param direction the direction in which to destroy the branch
+     */
+    public void destroyBranch(int x, int y, branchDirection direction) {
+        if (plantGrid[x][y].getBranch(direction) != null)
+            plantGrid[x][y].unmakeBranch(direction);
+    }
+
+    public boolean canGrowAt(float xArg, float yArg){
+        int xIndex = xCoordToIndex(xArg* 10);
+        int yIndex = yCoordToIndex(yArg * 10);
+        boolean lowerNode = xIndex % 2 == 0;
+        if(yIndex == 0 && lowerNode) return true;
+        int yOff = 0;
+        if(lowerNode) yOff = 1;
+
+        boolean below = false;
+        if(yIndex > 0) below = plantGrid[xIndex][yIndex-1].hasBranchInDirection(branchDirection.MIDDLE);
+        boolean downLeft = false;
+        if(xIndex >= 1) downLeft = plantGrid[xIndex-1][yIndex-yOff].hasBranchInDirection(branchDirection.RIGHT);
+        boolean downRight = false;
+        if(xIndex < width-1) downRight = plantGrid[xIndex+1][yIndex-yOff].hasBranchInDirection(branchDirection.LEFT);
+        System.out.println("Below: " + below + " downLeft: " + downLeft + " downRight: " + downRight);
+        return below || downLeft || downRight;
+    }
+
+    /**
+     * draws the current plant to the canvas
+     *
+     * @param canvas the canvas to draw to
+     */
+    public void draw(GameCanvas canvas) {
+        try {
+            for (PlantNode[] n : plantGrid) {
+                for (PlantNode node : n) {
+                    canvas.draw(nodeTexture,
+                            Color.WHITE,
+                            nodeTexture.getWidth() / 2f,
+                            nodeTexture.getHeight() / 2f,
+                            node.getX(),
+                            node.getY(),
+                            0.0f,
+                            1.0f,
+                            1.0f);
+                    try {
+                        node.drawBranches(canvas);
+                    } catch (Exception ignore) {
+                    }
+                }
+            }
+        } catch (Exception e) {
+        }
+    }
+
+    /**
+     * returns the number of nodes wide the plant is
+     */
+    public int getWidth() {
+        return width;
+    }
+
+    /**
+     * returns the number of nodes tall the plant is
+     */
+    public int getHeight() {
+        return height;
+    }
+
+    /**
+     * sets the texture to use when drawing nodes
+     */
+    public void gatherAssets(AssetDirectory directory) {
+
+        this.nodeTexture = directory.getEntry("shared:node", Texture.class);
+        this.branchTexture = new TextureRegion(directory.getEntry(
+                "shared:branch",
+                Texture.class));
+    }
+
+    /**
+     * returns whether or not a branch exists at the given x and y, in the given direction
+     *
+     * @param xArg      x coordinate of the queried node
+     * @param yArg      y coordinate of the queried node
+     * @param direction direction of the queried node
+     * @return boolean value of whether or not a branch exists here
+     */
+    public boolean branchExists(int xArg, int yArg, branchDirection direction) {
+        return plantGrid[xArg][yArg].hasBranchInDirection(direction);
+    }
+
+    /**
+     * Converts a screen x coordinate to the corresponding node index in the plant
+     *
+     * @param xArg x coordinate to be converted
+     * @return x index of the corresponding node
+     */
+    public int xCoordToIndex(float xArg) {
+        return Math.round((xArg - xOrigin) / xSpacing);
+    }
+
+    /**
+     * Converts a screen y coordinate to the corresponding node index in the plant
+     *
+     * @param yArg y coordinate to be converted
+     * @return y index of the corresponding node
+     */
+    public int yCoordToIndex(float yArg) {
+        return (int) ((yArg - yOrigin) / gridSpacing);
+    }
+
+    //TODO: Update with new branch type
+
+    /**
+     * enum containing directions in which a plant can grow from a node
+     */
+    public enum branchDirection {LEFT, MIDDLE, RIGHT}
+
+    /**
+     * enum containing possible branch types
+     */
+    public enum branchType {NORMAL}
+
+    /**
+     * enum containing possible leaf types
+     */
+    public enum leafType {NORMAL}
+
+    /**
+     * representation of a node in the plantGrid
+     */
+    public class PlantNode {
+        //TODO: change this class to contain branch objects instead of booleans
+        /**
+         * x coordinate of this node
+         */
+        private final float x;
+        /**
+         * y coordinate of this node
+         */
+        private final float y;
+        /**
+         * whether there is a branch in the leftmost slot of this node
+         */
+        private Branch left;
+        /**
+         * whether there is a branch in the middle slot of this node
+         */
+        private Branch middle;
+        /**
+         * whether there is a branch in the rightmost slot of this node
+         */
+        private Branch right;
+        /**
+         * the leaf for this node
+         */
+        private Leaf leaf;
+        /**
+         * width of the leaf at this node
+         */
+        private final int leafWidth = 60;
+        /**
+         * height of the leaf at this node
+         */
+        private final int leafHeight = 5;
+
+
+
+        /**
+         * initialize a new PlantNode object
+         *
+         * @param x x coordinate of the node
+         * @param y y coordinate of the node
+         */
+        public PlantNode(float x, float y) {
+            //            this.left = false;
+            //            this.middle = false;
+            //            this.right = false;
+            this.x = x;
+            this.y = y;
+        }
+
+        /**
+         * make a branch in the desired direction, of the desired type
+         *
+         * @param direction direction branch is facing
+         * @param type      type of branch to create
+         * @param texture   texture the branch should use
+         * @param world     world to assign the branch to
+         */
+        public void makeBranch(branchDirection direction,
+                                          branchType type,
+                                          TextureRegion texture,
+                                          World world) {
+            switch (direction) {
+                case LEFT:
+                case MIDDLE:
+                case RIGHT:
+            }
+
+        }
+
+        /**
+         * make a leaf of the desired type
+         * @param type type of leaf to create
+         * @param texture texture of the leaf
+         * @param world world to assign the leaf to
+         */
+        public void makeLeaf(leafType type, TextureRegion texture, World world) {
+            leaf = new Leaf(x, y, leafWidth, leafHeight);
+        }
+
+        /**
+         * destroy target branch
+         */
+        public void unmakeBranch(branchDirection direction) {
+            switch (direction){
+                case MIDDLE:
+                    middle.setDestroyed(true);
+                case RIGHT:
+                    right.setDestroyed(true);
+                case LEFT:
+                    left.setDestroyed(true);
+            }
+        }
+
+        //TODO: set return type to branch object instead of boolean
+
+        /**
+         * get the branch at a given direction
+         *
+         * @param direction whether getBranch returns the left, middle, or right branch
+         */
+        public Branch getBranch(branchDirection direction) {
+            switch (direction) {
+                case MIDDLE:
+                    return middle;
+                case LEFT:
+                    return left;
+                case RIGHT:
+                    return right;
+                default:
+                    return null;
+            }
+        }
+
+        /**
+         * returns the leaf of this node
+         * @return the leaf of this node
+         */
+        public Leaf getLeaf(){
+            return leaf;
+        }
+
+        /**
+         * returns the x position of this node
+         */
+        public float getX() {
+            return x;
+        }
+
+        /**
+         * returns the y position of this node
+         */
+        public float getY() {
+            return y;
+        }
+
+        public void drawBranches(GameCanvas canvas) {
+            if (left != null) left.draw(canvas);
+            if (right != null) right.draw(canvas);
+            if (middle != null) middle.draw(canvas);
+        }
+
+        /**
+         * if this node has a branch in the given direction
+         * @param direction the direction to check for a branch in
+         * @return whether the branch in this direction is not destroyed
+         */
+        public boolean hasBranchInDirection(branchDirection direction){
+            switch (direction) {
+                case MIDDLE:
+                    return middle != null && !middle.isDestroyed();
+                case LEFT:
+                    return left != null && !left.isDestroyed();
+                case RIGHT:
+                    return right != null && !right.isDestroyed();
+                default:
+                    return false;
+            }
+        }
+
+    }
+
 }
