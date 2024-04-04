837d7092003ab9ce41ac1af1c04bb488f9038222
Tawakalt <tbo8@cornell.edu>
2024-04-03 20:59:56 -0500

    Updated Functions in the Timer Class - Implemented startTime - Implemented displayTime - Implemented updateTime

--- core/src/com/syndic8/phytopolis/util/Timer.java
+++ core/src/com/syndic8/phytopolis/util/Timer.java
@@ -8,29 +8,47 @@ import com.syndic8.phytopolis.GameCanvas;
 
 public class Timer {
     private int minutes;
+
+    private int startSeconds;
+    private int startMinutes;
     private int seconds;
-    private int elapsedTime;
 
-    private int startTime;
+    private long startTime;
 
-    private String timeDisplay;
+    private boolean running;
     public Timer(int second){
-        seconds = second % 60;
-        minutes = second / 60;
+        startSeconds = second % 60;
+        startMinutes = second / 60;
+        seconds = startSeconds;
+        minutes = startMinutes;
     }
 
     public void startTimer(){
-        startTime = (int) TimeUtils.millis();
+        startTime = TimeUtils.millis();
+        running = true;
     }
 
     public void updateTime(){
-
+        if (running) {
+            long elapsedTime = TimeUtils.timeSinceMillis(startTime);
+            int totalSeconds = (int) (elapsedTime / 1000);
+            int remainingSeconds =
+                    startSeconds + startMinutes * 60 - totalSeconds;
+            if (remainingSeconds <= 0) {
+                running = false;
+                remainingSeconds = 0;
+            }
+            minutes = remainingSeconds / 60;
+            seconds = remainingSeconds % 60;
+        }
     }
 
     public void displayTime(GameCanvas canvas,
                             BitmapFont font, Color color,
                             float x, float y){
-
+        String timeDisplay = String.format("%02d:%02d", minutes, seconds);
+        font.setColor(color);
+        canvas.drawText(timeDisplay, font, x, y);
     }
     public void displayTime(GameCanvas canvas,
                             BitmapFont font, Color color,
@@ -39,6 +57,7 @@ public class Timer {
         font.getData().setScale(scale.x, scale.y);
         displayTime(canvas, font, color, x, y);
     }
+
 
 
 }
