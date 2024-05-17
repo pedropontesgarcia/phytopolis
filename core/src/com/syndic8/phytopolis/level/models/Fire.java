56e50469f95fa9cd72d26e4f86bdcbd3391b2024
Pedro Pontes García <pp457@cornell.edu>
2024-05-17 02:53:41 -0400

    Made fire grow in size a little faster

--- core/src/com/syndic8/phytopolis/level/models/Fire.java
+++ core/src/com/syndic8/phytopolis/level/models/Fire.java
@@ -11,7 +11,9 @@ import com.syndic8.phytopolis.util.Tilemap;
 public class Fire extends Hazard {
 
     private final int NUM_FRAMES = 16;
-
+    private final float growthRate = 0.0001f; // Growth rate per second
+    private float size = .4f; // Initial size
+    private float elapsedTime = 0.0f; // Elapsed time since last update
 
     /**
      * Constructs a Fire object with a specified location and duration.
@@ -19,7 +21,11 @@ public class Fire extends Hazard {
      * @param location The initial location of the fire.
      * @param duration The initial burn duration of the fire.
      */
-    public Fire(Vector2 pos, Vector2 location, int duration, Tilemap tm, float texScl) {
+    public Fire(Vector2 pos,
+                Vector2 location,
+                int duration,
+                Tilemap tm,
+                float texScl) {
         super(tm, texScl, pos, location, duration);
     }
 
@@ -31,30 +37,6 @@ public class Fire extends Hazard {
     @Override
     public ModelType getType() {
         return ModelType.FIRE;
-    }
-
-    private float size = .4f; // Initial size
-    private float growthRate = 0.00005f; // Growth rate per second
-    private float elapsedTime = 0.0f; // Elapsed time since last update
-
-    public void draw(GameCanvas canvas) {
-        float width = tilemap.getTileWidth() * textureSclInTiles;
-        float height = tilemap.getTileHeight() * textureSclInTiles;
-        float sclX = width / texture.getRegionWidth();
-        float sclY = height / texture.getRegionHeight();
-
-        getFilmStrip().setFrame((int) animFrame);
-        float x = getFilmStrip().getRegionWidth() / 2.0f;
-        float y = getFilmStrip().getRegionHeight() / 2.0f;
-        canvas.draw(getFilmStrip(),
-                Color.WHITE,
-                x,
-                y,
-                getX(),
-                getY(),
-                0,
-                sclX * size,
-                sclY * size);
     }
 
     public void update(float delta) {
@@ -70,6 +52,26 @@ public class Fire extends Hazard {
         float sizeIncrement = growthRate * elapsedTime;
         // Limit size to maximum of 1f
         size = Math.min(size + sizeIncrement, 1f);
+    }
+
+    public void draw(GameCanvas canvas) {
+        float width = tilemap.getTileWidth() * textureSclInTiles;
+        float height = tilemap.getTileHeight() * textureSclInTiles;
+        float sclX = width / texture.getRegionWidth();
+        float sclY = height / texture.getRegionHeight();
+
+        getFilmStrip().setFrame((int) animFrame);
+        float x = getFilmStrip().getRegionWidth() / 2.0f;
+        float y = getFilmStrip().getRegionHeight() / 2.0f;
+        canvas.draw(getFilmStrip(),
+                    Color.WHITE,
+                    x,
+                    y,
+                    getX(),
+                    getY(),
+                    0,
+                    sclX * size,
+                    sclY * size);
     }
 
 }
