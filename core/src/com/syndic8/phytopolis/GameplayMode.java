43ff60319b946f771b307e656d375c174aa17535
Ireanne Cao <ixc3@cornell.edu>
2024-05-17 23:23:08 -0400

    Branch limit testing

--- core/src/com/syndic8/phytopolis/GameplayMode.java
+++ core/src/com/syndic8/phytopolis/GameplayMode.java
@@ -416,11 +416,11 @@ public class GameplayMode extends WorldController {
 
         if (!hazardController.hasFire(unprojMousePos)) {
             if (shouldGrowBranch) {
-                if (timeSinceGrow >= 2.0f || numBranchesSinceGrow < 3) {
+                if (timeSinceGrow >= 0.4f || numBranchesSinceGrow < 1) {
                     Branch branch = plantController.growBranch(unprojMousePos.x,
                             unprojMousePos.y);
                     if (branch != null) {
-                        if (timeSinceGrow >= 2.0f) {
+                        if (timeSinceGrow >= 0.4f) {
                             System.out.println("BRANCH LIMIT RESET");
                             timeSinceGrow = 0;
                             numBranchesSinceGrow = 0;
